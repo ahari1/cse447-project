8623d2a50d5d363ca43899bbae2212395dbb96a6
Eric Ye <yuxuan.smiley@gmail.com>
2025-03-10 09:55:42 -0700

    Fix Typo

--- ctest/test.cc
+++ ctest/test.cc
@@ -19,7 +19,7 @@ constexpr int CACHE_THRESHOLD = 100;
 
 class FastTrie {
 public:
-    FastTrie(const vector<string>& keys) : query_cache(CAChE_SIZE) {
+    FastTrie(const vector<string>& keys) : query_cache(CACHE_SIZE) {
         int n = keys.size();
         map<string, int> key_to_token_id;
         for (int i = 0; i < n; ++i) {
